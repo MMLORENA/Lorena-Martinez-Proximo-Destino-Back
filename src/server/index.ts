64909e12868007c1eaa3aa6b9d5cfa3f7ad40a1e
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-08-29 20:58:08 +0200

    exclude from coverage loadEnviroment & server index

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import generalError from "./middlewares/error";
 
 const app = express();
 app.disable("x-powered-by");
 
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use(generalError);
 
 export default app;
