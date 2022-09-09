239adbc1702d1be9970c8e77c6342e1cec7f4828
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-09-09 22:26:51 +0200

    provisional url & key create storage superbase

--- src/server/middlewares/storageImages/supaBaseClient.ts
+++ src/server/middlewares/storageImages/supaBaseClient.ts
@@ -2,8 +2,8 @@ import "../../../loadEnvironment";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  "https://eisgrvsdasqjohkcrlpz.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpc2dydnNkYXNxam9oa2NybHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI3NDMzMjQsImV4cCI6MTk3ODMxOTMyNH0.riUUXGyWc1wwoR_gLP-PynLUWwZcZBrUx2hytJQXtqU"
 );
 
 export default supabase;
